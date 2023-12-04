03db8f2f372310108d712a9ef218470d29a88d8a
Javokhir Jambulov <91411930+JavokhirJambulov@users.noreply.github.com>
2023-12-04 22:41:28 +0900

    Update ros2_android_app_pytest.py

--- Javokhir 12204567/ros2_android_app/ros2_android_app_pytest.py
+++ Javokhir 12204567/ros2_android_app/ros2_android_app_pytest.py
@@ -2,7 +2,7 @@
 
 import pytest
 from app import MainActivity  
-from your_ros_publisher_module import Publisher, Message, BaseEntity
+import Publisher, Message, BaseEntity
 
 @pytest.fixture(scope="class")
 def app(request, android_driver):
@@ -14,15 +14,9 @@ def app(request, android_driver):
 @pytest.mark.usefixtures("app")
 class TestJoystickFunctionality:
     def test_joystick_to_ros_message(self):
-        # Assume you have a function to send joystick data as a ROS message
         joystick_data = {"axis_x": 0.5, "axis_y": -0.3}
-        ros_publisher = Publisher(Message)  # Replace Message with the actual message type
+        ros_publisher = Publisher(Message)  
 
         # Call the toRosMessage function
         self.app.toRosMessage(ros_publisher, joystick_data)
 
-        # Add assertions or checks based on the expected behavior
-        assert ros_publisher.has_received_data()  # Replace with the actual method to check if data has been received
-        assert ros_publisher.get_last_received_data() == expected_ros_message  # Replace with the expected ROS message
-
-    # Add more test cases as needed
